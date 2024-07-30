f396015dd92324d92ed98d28171578c286bd558f
nodus8tollens <mladenovskipredrag@gmail.com>
2024-07-30 18:53:16 +0200

    accepts command line arguments for source, replica, and interval

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
+import argparse
+import time
 from sync import sync_folders
 
 def main():
-    source_folder = "./source"
-    replica_folder = "./replica"
-    sync_folders(source=source_folder, replica=replica_folder)
+    parser = argparse.ArgumentParser(description="Synchronize source and replica folders.")
+    parser.add_argument("source_folder", type=str, help="The source folder path")
+    parser.add_argument("replica_folder", type=str, help="The replica folder path")
+    parser.add_argument("interval", type=int, help="Time interval for synchronization expressed in seconds")
+
+    args = parser.parse_args()
+
+    source_folder = args.source_folder
+    replica_folder = args.replica_folder
+    interval = args.interval
+
+    while True:
+        sync_folders(source=source_folder, replica=replica_folder)
+        time.sleep(interval)
 
 
 if __name__ == "__main__":
